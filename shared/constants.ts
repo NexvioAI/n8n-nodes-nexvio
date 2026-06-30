d0523fa169c16d1361682223a7d4e1adc3a274bb
Harsh W3Dev <harsh.j@w3dev.email>
2026-06-30 21:04:25 +0000

    update scopes

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,4 +1,4 @@
 export { NEXVIO_DEFAULT_DASHBOARD_URL } from "./oauth-config"
 
 export const NEXVIO_OAUTH_SCOPES =
-  "read:contacts write:contacts read:conversations write:conversations read:agents write:agents"
+  "read:contacts write:contacts read:conversations write:conversations read:agents write:agents read:forms write:forms write:tickets"
